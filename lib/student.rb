64db871dda708e78c9473bb6f44378815d8f7250
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-03 02:35:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,8 +15,13 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+      SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      Student.new_from_db(row)
+    end.first
   end
 
   def save
