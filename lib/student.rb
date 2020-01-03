0230ff4b3cf5cf922350de156a7f4b53c07cf5e3
Russell Macalintal <russell.macalintal@gmail.com>
2020-01-03 02:48:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,14 @@ class Student
     end
   end
 
+  def self.all_students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < '12'"
+
+    DB[:conn].execute(sql).map do |row|
+      Student.new_from_db(row)
+    end
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM students WHERE name = ? LIMIT 1
